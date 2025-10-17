6a1d59ddb867e7348b07136c24504daac3456ae0
tired_ <omaralim06@gmail.com>
2025-10-17 01:11:36 -0700

    feat: document code

--- src/blog.ts
+++ src/blog.ts
@@ -1,3 +1,4 @@
+// creating blog type
 type Blog = {
     title: string;
     date: string;
@@ -7,6 +8,7 @@ type Blog = {
     slug: string;
 }
 
+// creating blogs list
 const blogs: Blog[] = [
     {
         title: "Completing the starter pack",
@@ -26,34 +28,42 @@ const blogs: Blog[] = [
     }
 ]
 
+// acessing and storing blog-container div
 const blogContainer = document.getElementById('blog-container');
+
 //const blogContainer = document.getElementById('blog-container')!;
-// non-null assertion to bypass Ts null checking, alternative way
+    // non-null assertion to bypass Ts null checking, alternative way
 
-
+// load up blogs from blog list
 blogs.forEach((Blog) => {
     // creating the overall blog element
     const blog = document.createElement("div");
+
     // adding a class to the blog div
     blog.classList.add("blog");
-    // creating the elements an that will be inside the blog div
-    // and assigning their contents
+
+    // creating the blog div's children
+        // and assigning their contents
     const h1 = document.createElement("h1");
     h1.textContent = Blog.title;
+
     const img = document.createElement("img");
     img.src = Blog.image;
     img.alt = Blog.imageAlt;
     img.width = 700;
+
     const imgLink = document.createElement("a");
     imgLink.href = Blog.slug;
+    // wrapping img within the anchor element
     imgLink.appendChild(img);
+
     const p = document.createElement("p");
     p.textContent = Blog.description
+
     // appending as children to the blog div
     blog.appendChild(h1);
     blog.appendChild(imgLink);  
     blog.appendChild(p);
-
 
     //appending the blog div to the container div
     // null checking to avoid Ts error
