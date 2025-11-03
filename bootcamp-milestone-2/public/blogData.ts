f0e4530064fd9cc88d360ed02c4796d618210313
tired_ <omaralim06@gmail.com>
2025-11-03 03:13:17 -0800

    feat: blog page react implementation initial commit

---
+++ bootcamp-milestone-2/public/blogData.ts
@@ -0,0 +1,31 @@
+// creating blog type
+export type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+// creating blogs list
+const blogs: Blog[] = [
+    {
+        title: "Completing the starter pack",
+        date: "04-11-2025",
+        description: "blog post that talks about completing the hack4impact starter pack",
+        image: "website.png",
+        imageAlt: "screenshot of personal website",
+        slug: "blog/completing-the-starter-pack.html"
+    },
+    {
+        title: "Learning Typescript",
+        date: "14-11-2025",
+        description: "very first blog post about learning typescript",
+        image: "typescript1.png",
+        imageAlt: "screenshot of vscode typescript project",
+        slug: "blog/learning-typescript.html"
+    }
+];
+
+export default blogs;
---
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import NavBar from '@/components/navbar';
+import PageTitle from '@/components/pageTitle';
+
+
+export default function Blog(){
+    return(
+        <div>
+            <PageTitle title="My Blog"/>
+            <div id="blog-container"></div>
+            
+        </div>
+    );
+}
