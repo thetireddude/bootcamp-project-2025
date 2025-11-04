8d65ef4e2d4a81478bd4d9b905a5457917787358
tired_ <omaralim06@gmail.com>
2025-11-03 16:41:10 -0800

    style: blog page

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -7,11 +7,18 @@ import Image from "next/image";
 export default function BlogPreview(blog : Blog){
     return(
         <div className={style.blog}>
-            <h1>{blog.title}</h1>
-            <a href={blog.slug}>
-                <Image src={blog.image} alt={blog.imageAlt} width={700} height={500}></Image>
-            </a>
-            <p>{blog.description}</p>
+
+            <div className={style.blogImage}>
+                <a href={blog.slug}>
+                    <Image src={blog.image} alt={blog.imageAlt} width={400} height={300}></Image>
+                </a>
+            </div>
+
+            <div className={style.blogAbout}>
+                <h1>{blog.title}</h1>
+                <span className={style.date}>{blog.date}</span>
+                <p>{blog.description}</p>
+            </div>
         </div>
     );
 }
